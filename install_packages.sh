c98ca0a3cb20285bf60ce9ec8cf8e5bb30b663ac
Khoa Duong <dnakhoa@icloud.com>
2023-01-29 05:53:33 +0900

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-pip install tqdm
-pip install spacy
-python3 -m spacy download en_core_web_lg
-pip install joblib
-pip install numpy
-pip install pandas
+pip install tqdm==4.61.1
+#pip install spacy
+#python3 -m spacy download en_core_web_lg
+pip install joblib==1.0.1
+pip install numpy==1.20.3
+#pip install pandas
 pip install torch==1.8.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html
-pip install pytorch-lightning
-pip install transformers sentencepiece
-pip install loguru
+pip install pytorch-lightning==1.3.5
+pip install transformers==4.14.1
+pip install sentencepiece==0.1.95
+pip install loguru==0.5.3
